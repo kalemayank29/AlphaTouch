873021dda3de2e120533816ce25619534cc774e5
Mayank Kale <kalemayank29@gmail.com>
2014-06-11 11:56:45 -0500

    Initial Commit

---
+++ AlphaTouch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlphaTouch
+//
+//  Created by Mayank Kale on 6/11/14.
+//  Copyright (c) 2014 Mayank Kale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AlphaTouch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlphaTouch
+//
+//  Created by Mayank Kale on 6/11/14.
+//  Copyright (c) 2014 Mayank Kale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
